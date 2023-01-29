d5c443a59c8ba3e0d67aa803bdd6edf19e5f359f
Sidorova Tania <sidorovatania3@gmail.com>
2023-01-29 11:51:55 +0300

    Задача 36 решение

--- 36.py
+++ 36.py
@@ -17,8 +17,9 @@
 
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
-    print('\n'.join([' '.join(map(str, [operation(i, j) for j in range(
-        1, num_columns+1)])) for i in range(1, num_rows+1)]))
+    for x in range(1, num_rows + 1):
+        for y in range(1, num_columns + 1):
+            print(operation(x, y), end='\t')
+        print()
 
-
-print_operation_table(lambda x, y: x * y) 
+print_operation_table(lambda x, y: x * y)
